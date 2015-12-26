958e024f69c862dbfc4432d8ee6556188fd74ef2
sethdorris <sethc.dorris@gmail.com>
2015-12-25 22:30:03 -0600

    Created repo tests, setting up my initial test method, cleanup method, and connect mocks to data store method. Created first test of GetAllPostsMethod and it passed.

---
+++ MyBlog.Tests/Models/BlogRepoTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyBlog.Models;
+using Moq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MyBlog.Tests.Models
+{
+    [TestClass]
+    public class BlogRepoTests
+    {
+        private Mock<BlogContext> _mycontext;
+        private Mock<DbSet<Post>> _postSet;
+        private Mock<DbSet<User>> _userSet;
+        private BlogRepo _myrepo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mycontext = new Mock<BlogContext>();
+            _postSet = new Mock<DbSet<Post>>();
+            _userSet = new Mock<DbSet<User>>();
+            _myrepo = new BlogRepo(_mycontext.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _mycontext = null;
+            _postSet = null;
+            _userSet = null;
+            _myrepo = null;
+        }
+
+        [TestMethod]
+        public void ConnectMocksToDataStore(IEnumerable<Post> data_source)
+        {
+            var data = data_source.AsQueryable();
+            _postSet.As<IQueryable<Post>>().Setup(p => p.Provider).Returns(data.Provider);
+            _postSet.As<IQueryable<Post>>().Setup(p => p.Expression).Returns(data.Expression);
+            _postSet.As<IQueryable<Post>>().Setup(p => p.ElementType).Returns(data.ElementType);
+            _postSet.As<IQueryable<Post>>().Setup(p => p.GetEnumerator()).Returns(data.GetEnumerator());
+            _mycontext.Setup(p => p.Posts).Returns(_postSet.Object);
+        }
+
+
+        [TestMethod]
+        public void BlogRepoTestsEnsureICanGetAllPosts()
+        {
+            List<Post> expected = new List<Post>
+            {
+                new Post { PostID = 1, Title = "NEWS" },
+                new Post { PostID = 2, Title = "NEWS2" }
+            };
+
+            ConnectMocksToDataStore(expected);
+            List<Post> actual = _myrepo.GetAllPosts();
+            Assert.AreEqual(expected[0].PostID, actual[0].PostID);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
