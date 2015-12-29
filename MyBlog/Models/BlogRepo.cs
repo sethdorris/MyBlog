e17fac0a1617d66f6a8f051b95cd550cf9bc2815
sethdorris <sethc.dorris@gmail.com>
2015-12-28 22:13:02 -0600

    Added a method to search posts by content and order by post date.

--- MyBlog/Models/BlogRepo.cs
+++ MyBlog/Models/BlogRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace MyBlog.Models
@@ -30,6 +31,13 @@ namespace MyBlog.Models
         public List<User> GetUserByHandle(string handle)
         {
             var query = from users in context.BlogUsers.Where(o => o.Handle.Contains(handle)) select users;
+            return query.ToList();
+        }
+
+        public List<Post> SearchPostsByContent(string content)
+        {
+            var query = from posts in context.Posts.Where(data => Regex.IsMatch(data.Content, content, RegexOptions.IgnoreCase)).ToList() select posts;
+            query.OrderBy(o => o.DateTime);
             return query.ToList();
         }
     }
